3208b2eee153af305bde1ec96ec8aa2551a7ef0d
Dhruv282011 <95422793+Dhruv282011@users.noreply.github.com>
2022-01-18 19:04:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("hy i am a console screen")
+var firstname="Dhruv";
+console.log(firstname);
+var lastname="sharma"
+console.log(lastname);
+var name =firstname.concat(lastname) 
+console.log(name)
+console.log(firstname.length)
+console.log(lastname.length)
+console.log(name.length)
+console.log(firstname.toUpperCase())
+var num1 = Math.sqrt(144)
+console.log(num1)
